66e1b92586a97277c5d40fccba6b0881a81cc807
Ncr2001 <chandanansr2001@gmail.com>
2023-12-02 10:04:11 +0530

    Added the arthematic File

---
+++ src/ncr/rmp/Arthmetic.java
@@ -0,0 +1,14 @@
+package ncr.rmp;
+
+public class Arthmetic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 22;
+		int b =36;
+        System.out.println("A+B="+(a+b));
+        System.out.println("A-B="+(a-b));
+        System.out.println("A*B="+(a*b));
+	}
+
+}
